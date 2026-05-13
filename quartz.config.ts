7861bd267d64fd6fec460316367215ed383e9fce
AailaaForest <284262404+aailaaforest@users.noreply.github.com>
2026-05-13 17:01:57 +0500

    Fix baseUrl for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "FamForest.github.io/aailaaforest",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
